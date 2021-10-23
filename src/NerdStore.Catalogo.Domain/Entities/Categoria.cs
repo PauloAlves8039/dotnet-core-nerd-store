ab7aff703b9139336f4a621c4f6b4a9e85c5d25b
PauloAlves8039 <pj38alves@gmail.com>
2021-10-22 21:11:57 -0300

    Implementação inicial das entidades

---
+++ src/NerdStore.Catalogo.Domain/Entities/Categoria.cs
@@ -0,0 +1,10 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalogo.Domain.Entities
+{
+    public class Categoria : Entity
+    {
+        public string Nome { get; private set; }
+        public int Codigo { get; private set; }
+    }
+}
