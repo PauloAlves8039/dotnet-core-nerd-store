02fa6aed2aeabac7a5fabfe5d162db2c0c071e6a
PauloAlves8039 <pj38alves@gmail.com>
2021-10-23 14:19:38 -0300

    Criando validações

--- src/NerdStore.Catalogo.Domain/Entities/Produto.cs
+++ src/NerdStore.Catalogo.Domain/Entities/Produto.cs
@@ -23,6 +23,8 @@ namespace NerdStore.Catalogo.Domain.Entities
             Valor = valor;
             DataCadastro = dataCadastro;
             Imagem = imagem;
+
+            Validar();
         }
         
         public void Ativar() => Ativo = true;
@@ -37,12 +39,14 @@ namespace NerdStore.Catalogo.Domain.Entities
 
         public void AlterarDescricao(string descricao)
         {
+            Validacoes.ValidarSeVazio(descricao, "O campo Descricao do produto não pode estar vazio");
             Descricao = descricao;
         }
 
         public void DebitarEstoque(int quantidade)
         {
             if (quantidade < 0) quantidade *= -1;
+            if (!PossuiEstoque(quantidade)) throw new DomainException("Estoque insuficiente");
             QuantidadeEstoque -= quantidade;
         }
 
@@ -58,6 +62,11 @@ namespace NerdStore.Catalogo.Domain.Entities
 
         public void Validar()
         {
+            Validacoes.ValidarSeVazio(Nome, "O campo Nome do produto não pode estar vazio");
+            Validacoes.ValidarSeVazio(Descricao, "O campo Descricao do produto não pode estar vazio");
+            Validacoes.ValidarSeIgual(CategoriaId, Guid.Empty, "O campo CategoriaId do produto não pode estar vazio");
+            Validacoes.ValidarSeMenorQue(Valor, 1, "O campo Valor do produto não pode se menor igual a 0");
+            Validacoes.ValidarSeVazio(Imagem, "O campo Imagem do produto não pode estar vazio");
         }
     }
 }
