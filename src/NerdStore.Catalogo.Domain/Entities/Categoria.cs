137f2ec00f5c9b5c4aeb2c58f6350d38559f16f9
PauloAlves8039 <pj38alves@gmail.com>
2021-10-23 13:51:00 -0300

    Criando elementos para as entidades Produto e Categoria

--- src/NerdStore.Catalogo.Domain/Entities/Categoria.cs
+++ src/NerdStore.Catalogo.Domain/Entities/Categoria.cs
@@ -6,5 +6,16 @@ namespace NerdStore.Catalogo.Domain.Entities
     {
         public string Nome { get; private set; }
         public int Codigo { get; private set; }
+
+        public Categoria(string nome, int codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Codigo}";
+        }
     }
 }
--- src/NerdStore.Catalogo.Domain/Entities/Produto.cs
+++ src/NerdStore.Catalogo.Domain/Entities/Produto.cs
@@ -14,5 +14,50 @@ namespace NerdStore.Catalogo.Domain.Entities
         public string Imagem { get; private set; }
         public int QuantidadeEstoque { get; private set; }
         public Categoria Categoria { get; private set; }
+
+        public Produto(string nome, string descricao, bool ativo, decimal valor, Guid categoriaId, DateTime dataCadastro, string imagem)
+        {
+            Nome = nome;
+            Descricao = descricao;
+            Ativo = ativo;
+            Valor = valor;
+            DataCadastro = dataCadastro;
+            Imagem = imagem;
+        }
+        
+        public void Ativar() => Ativo = true;
+
+        public void Desativar() => Ativo = false;
+
+        public void AlterarCategoria(Categoria categoria)
+        {
+            Categoria = categoria;
+            CategoriaId = categoria.Id;
+        }
+
+        public void AlterarDescricao(string descricao)
+        {
+            Descricao = descricao;
+        }
+
+        public void DebitarEstoque(int quantidade)
+        {
+            if (quantidade < 0) quantidade *= -1;
+            QuantidadeEstoque -= quantidade;
+        }
+
+        public void ReporEstoque(int quantidade)
+        {
+            QuantidadeEstoque += quantidade;
+        }
+
+        public bool PossuiEstoque(int quantidade)
+        {
+            return QuantidadeEstoque >= quantidade;
+        }
+
+        public void Validar()
+        {
+        }
     }
 }
