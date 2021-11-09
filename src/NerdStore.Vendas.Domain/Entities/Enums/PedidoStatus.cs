937062411f77d4334f5180928a295fa7957b23d5
PauloAlves8039 <pj38alves@gmail.com>
2021-11-08 21:10:52 -0300

    Definindo lógica nos domínios de Vendas

--- src/NerdStore.Vendas.Domain/Entities/Enums/PedidoStatus.cs
+++ src/NerdStore.Vendas.Domain/Entities/Enums/PedidoStatus.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace NerdStore.Vendas.Domain.Entities.Enums
+﻿namespace NerdStore.Vendas.Domain.Entities.Enums
 {
     public enum PedidoStatus
     {
+        Rascunho = 0,
+        Iniciado = 1,
+        Pago = 4,
+        Entregue = 5,
+        Cancelado = 6
     }
 }
--- src/NerdStore.Vendas.Domain/Entities/Enums/TipoDescontoVoucher.cs
+++ src/NerdStore.Vendas.Domain/Entities/Enums/TipoDescontoVoucher.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace NerdStore.Vendas.Domain.Entities.Enums
+﻿namespace NerdStore.Vendas.Domain.Entities.Enums
 {
     public enum TipoDescontoVoucher
     {
+        Porcentagem = 0,
+        Valor = 1
     }
 }
